2a3b7448b4b59b0dea05a6821c78d1e95fda1d19
Vasyl-15.01.11 <154671975+Vasyajecvdcv@users.noreply.github.com>
2024-04-13 12:47:26 +0300

    Update editor.py

--- editor.py
+++ editor.py
@@ -2,7 +2,7 @@ from PyQt5.QtWidgets import QMainWindow
 from PyQt5 import QtCore,QtGui,QtWidgets
 from ui import Ui_MainWindow
 import os
-
+from PIL import Image,ImageFilter,ImageEnhance
 
 class ImageEditor(QMainWindow):
     def __init__(self):
@@ -11,7 +11,136 @@ class ImageEditor(QMainWindow):
         self.ui.setupUi(self)
         self.workdir=""
         self.ui.btn_dir.clicked.connect(self.open)
+        self.filename=None
+        self.photo=None
+        self.save_dir="changed"
+        self.ui.list_files.itemClicked.connect(self.showchosenimage)
+        self.ui.btn_bw.clicked.connect(self.do_bw)
+        self.ui.btn_blur.clicked.connect(self.do_blur)
+        self.ui.btn_left.clicked.connect(self.do_left)
+        self.ui.btn_right.clicked.connect(self.do_right)
+        self.ui.mirror_1.clicked.connect(self.do_mirror)
+        self.ui.btn_contrast.clicked.connect(self.do_contrast)
+        self.ui.btn_1.clicked.connect(self.do_mirror_tb)
+        self.ui.btn_2.clicked.connect(self.do_diagonal)
+    def loadimage(self, filename):
+        try:
+            self.filename=filename
+            path=os.path.join(self.workdir,self.filename)
+            self.photo=Image.open(path)
+        except:
+            win=QtWidgets.QMessageBox()
+            win.setText("Не вдалося відкрити зображення!")
+            win.exec()
 
+    def do_bw(self):
+        self.photo=self.photo.convert("L")
+        self.saveimage()
+        image_path=os.path.join(self.workdir,self.save_dir,self.filename)
+        self.showimage(image_path)
+
+    
+
+    def saveimage(self):
+        path=os.path.join(self.workdir,self.save_dir)
+        if not(os.path.exists(path) or os.path.isdir(path)):
+            os.mkdir(path)
+        new_path=os.path.join(path,self.filename)
+        self.photo.save(new_path)
+
+    def showimage(self,path):
+        self.ui.image.hide()
+        pix=QtGui.QPixmap(path)
+        w,h=self.ui.image.width(),self.ui.image.height()
+        pix=pix.scaled(w,h,QtCore.Qt.KeepAspectRatio)
+        self.ui.image.setPixmap(pix)
+        self.ui.image.show()
+
+
+    def do_blur(self):
+        try:
+            self.photo=self.photo.filter(ImageFilter.BLUR)
+            self.saveimage()
+            image_path=os.path.join(self.workdir,self.save_dir,self.filename)
+            self.showimage(image_path)
+        except:
+            win=QtWidgets.QMessageBox()
+            win.setText("Не обрано фото для редагування!")
+            win.exec()
+
+    def do_left(self):
+        try:
+            self.photo=self.photo.transpose(Image.ROTATE_90)
+            self.saveimage()
+            image_path=os.path.join(self.workdir,self.save_dir,self.filename)
+            self.showimage(image_path)
+        except:
+            win=QtWidgets.QMessageBox()
+            win.setText("Не обрано фото для редагування!")
+            win.exec()
+
+    def do_right(self):
+        try:
+            self.photo=self.photo.transpose(Image.ROTATE_270)
+            self.saveimage()
+            image_path=os.path.join(self.workdir,self.save_dir,self.filename)
+            self.showimage(image_path)
+        except:
+            win=QtWidgets.QMessageBox()
+            win.setText("Не обрано фото для редагування!")
+            win.exec()
+
+    def do_mirror(self):
+        try:
+            self.photo=self.photo.transpose(Image.FLIP_LEFT_RIGHT)
+            self.saveimage()
+            image_path=os.path.join(self.workdir,self.save_dir,self.filename)
+            self.showimage(image_path)
+        except:
+            win=QtWidgets.QMessageBox()
+            win.setText("Не обрано фото для редагування!")
+            win.exec()
+
+    def do_contrast(self):
+        try:
+            self.photo=ImageEnhance.Contrast(self.photo).enhance(1.5)
+            self.saveimage()
+            image_path=os.path.join(self.workdir,self.save_dir,self.filename)
+            self.showimage(image_path)
+        except:
+            win=QtWidgets.QMessageBox()
+            win.setText("Не обрано фото для редагування!")
+            win.exec()
+
+    def do_mirror_tb(self):
+        try:
+            self.photo=self.photo.transpose(Image.FLIP_TOP_BOTTOM)
+            self.saveimage()
+            image_path=os.path.join(self.workdir,self.save_dir,self.filename)
+            self.showimage(image_path)
+        except:
+            win=QtWidgets.QMessageBox()
+            win.setText("Не обрано фото для редагування!")
+            win.exec()
+
+    def do_diagonal(self):
+        try:
+            self.photo=self.photo.rotate(45)
+            self.saveimage()
+            image_path=os.path.join(self.workdir,self.save_dir,self.filename)
+            self.showimage(image_path)
+        except:
+            win=QtWidgets.QMessageBox()
+            win.setText("Не обрано фото для редагування!")
+            win.exec()
+
+    def showchosenimage(self):
+        if self.ui.list_files.selectedItems():
+            filename=self.ui.list_files.selectedItems()[0].text()
+            self.loadimage(filename)
+            path=os.path.join(self.workdir,filename)
+            self.showimage(path)
+        
 
     def open(self):
         try:
@@ -27,7 +156,7 @@ class ImageEditor(QMainWindow):
             alert.setText("Шлях до папки не вибрано!")
             alert.exec()
     def sort_files(self,files):
-        extentions=[".jpg",".svg",".png",".jpeg"]
+        extentions=[".jpg",".bmp",".png",".jpeg"]
         result=[]
         for file in files:
             for ex in extentions:
